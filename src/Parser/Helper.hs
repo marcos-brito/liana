4c9f390e901726aac60b89f129c59cc250ff1954
Marcos Brito <marcos.brito0507@gmail.com>
2025-11-12 12:52:39 -0300

    refactor: use TokenParser to create helper parsers

--- src/Parser/Helper.hs
+++ src/Parser/Helper.hs
@@ -1,13 +1,72 @@
 module Parser.Helper where
 
 import Parser.Base
-import Text.Parsec
+import Syntax.Base
+import Text.Parsec.Language (emptyDef)
+import qualified Text.Parsec.Token as P
 
-parens :: Parser a -> Parser a
-parens = between (char '(') (char ')')
+langDef :: P.LanguageDef ()
+langDef =
+  emptyDef
+    { P.reservedNames =
+        [ "val",
+          "var",
+          "data",
+          "fun",
+          "return",
+          "signature",
+          "instance",
+          "of",
+          "when",
+          "loop",
+          "as",
+          "if",
+          "else",
+          "false",
+          "true",
+          "this",
+          "imp",
+          "exp"
+        ],
+      P.reservedOpNames =
+        [ "+",
+          "-",
+          "*",
+          "/",
+          "%",
+          "&&",
+          "||",
+          ">",
+          ">=",
+          "<",
+          "<=",
+          "=",
+          "==",
+          "!=",
+          "->"
+        ]
+    }
+
+lexer :: P.TokenParser ()
+lexer = P.makeTokenParser langDef
+
+symbol :: String -> Parser String
+symbol = P.symbol lexer
+
+reserved :: String -> Parser ()
+reserved = P.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = P.reservedOp lexer
 
 brackets :: Parser a -> Parser a
-brackets = between (char '[') (char ']')
+brackets = P.brackets lexer
+
+parens :: Parser a -> Parser a
+parens = P.parens lexer
 
 braces :: Parser a -> Parser a
-braces = between (char '{') (char '}')
+braces = P.braces lexer
+
+commaSep :: Parser a -> Parser [a]
+commaSep = P.commaSep lexer
